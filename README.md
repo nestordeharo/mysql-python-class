d7f80e4c60bf89140698f3b3c5b7bf8f7fba8564
Nestor Mac Os X <nestordeharo@rigs.com.mx>
2015-01-03 16:24:16 -0600

    Include python class
    We include the python class that you could use to save time for
    connecting with MySQL servers

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 MySQL Python Class
+===========
 
 This is a Python class for connecting with MySQL using MySQLdb
 
-*Requirements
+- Requirements
 
 MySQL-python >= 1.2.5
 
-*Description
+- Description
 
 Useful for SELECT, INSERT, DELETE, UPDATE and use a custom function: SELECT_ADVANCED --> Query information with more than one parameter.
---
+++ mysql-python.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import MySQLdb
+from collections import OrderedDict
+
+class PdoMysql(object):
+    """
+        Class for connecting  with MySQL server
+    """
+
+    __instance   = None
+    __host       = None
+    __user       = None
+    __password   = None
+    __database   = None
+    __session    = None
+    __connection = None
+
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance or not cls.__database:
+             cls.__instance = super(PdoMysql, cls).__new__(cls,*args,**kwargs)
+        return cls.__instance
+    ## End def __new__
+
+    def __init__(self, host='localhost', user='root', password='', database=''):
+        self.__host     = host
+        self.__user     = user
+        self.__password = password
+        self.__database = database
+    ## End def __init__
+
+    def __open(self):
+        try:
+            cnx = MySQLdb.connect(self.__host, self.__user, self.__password, self.__database)
+            self.__connection = cnx
+            self.__session    = cnx.cursor()
+        except MySQLdb.Error as e:
+            print "Error %d: %s" % (e.args[0],e.args[1])
+    ## End def __open
+
+    def __close(self):
+        self.__session.close()
+        self.__connection.close()
+    ## End def __close
+
+    def select(self, table, where=None, *args, **kwargs):
+        result = None
+        query = 'SELECT '
+        keys = args
+        values = tuple(kwargs.values())
+        l = len(keys) - 1
+
+        for i, key in enumerate(keys):
+            query += "`"+key+"`"
+            if i < l:
+                query += ","
+        ## End for keys
+
+        query += 'FROM %s' % table
+
+        if where:
+            query += " WHERE %s" % where
+        ## End if where
+
+        self.__open()
+        self.__session.execute(query, values)
+        number_rows = self.__session.rowcount
+        number_columns = len(self.__session.description)
+
+        if number_rows >= 1 and number_columns > 1:
+            result = [item for item in self.__session.fetchall()]
+        else:
+            result = [item[0] for item in self.__session.fetchall()]
+        self.__close()
+
+        return result
+    ## End def select
+
+    def update(self, table, where=None, **kwargs):
+        query  = "UPDATE %s SET " % table
+        keys   = kwargs.keys()
+        values = tuple(kwargs.values())
+
+        l = len(keys) - 1
+        for i, key in enumerate(keys):
+            query += "`"+key+"` = %s"
+            if i < 1:
+                query += ","
+            ## End if i less than 1
+            query += "WHERE %s " % where
+        ## End for keys
+        self.__open()
+        self.__session.execute(query, values)
+        self.__connection.commit()
+        self.__close()
+    ## End def update
+
+    def insert(self, table, *args, **kwargs):
+        values = None
+        query = "INSERT INTO %s " % table
+        if kwargs:
+            keys = kwargs.keys()
+            values = tuple(kwargs.values())
+            query += "(" + ",".join(["`%s`"] * len(keys)) %  tuple (keys) + ") VALUES (" + ",".join(["%s"]*len(values)) + ")"
+        elif args:
+            values = args
+            query += " VALUES(" + ",".join(["%s"]*len(values)) + ")"
+
+        self.__open()
+        self.__session.execute(query, values)
+        self.__connection.commit()
+        self.__close()
+        return self.__session.lastrowid
+    ## End def insert
+
+    def delete(self, table, where=None):
+        query = "DELETE FROM %s WHERE %s" % (table, where)
+        self.__open()
+        self.__session.execute(query)
+        self.__connection.commit()
+        self.__close()
+    ## End def delete
+
+    def select_advanced(self, sql, *args):
+        od = OrderedDict(args)
+        query  = sql
+        values = tuple(od.values())
+        self.__open()
+        self.__session.execute(query, values)
+        number_rows = self.__session.rowcount
+        number_columns = len(self.__session.description)
+
+        if number_rows >= 1 and number_columns > 1:
+            result = [item for item in self.__session.fetchall()]
+        else:
+            result = [item[0] for item in self.__session.fetchall()]
+
+        self.__close()
+        return result
+    ## End def select_advanced
+## End class
